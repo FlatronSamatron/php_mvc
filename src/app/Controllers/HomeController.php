e80d38a94f4109688b205a8edeb09231f31361b0
Vitalii <flatronsamatron@gmail.com>
2024-02-06 13:48:07 +0200

    add view and controller

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Classes;
+namespace App\Controllers;
 
 class Home{
     public function index(): string{
--- src/app/Controllers/InvoiceController.php
+++ src/app/Controllers/InvoiceController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Classes;
+namespace App\Controllers;
 
 class Invoice{
     public function index(): string{
